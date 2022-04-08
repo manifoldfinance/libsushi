5db33ed2523708164a9489937db4d540f929f6dc
sam bacha <sam@manifoldfinance.com>
2022-04-08 06:40:08 -0700

    fix(txstate): update

--- src/SushiGuard/index.ts
+++ src/SushiGuard/index.ts
@@ -1,13 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+
 import { JsonRpcResponse } from '../JsonRpc/index';
 
 /**
- * Basic explanation of the tx state types:
+ * @package OpenMevTxState
+ * @version 2022.04
+ * @see {@link docs.openmev.org}
+ * @notice This is a flashbots-api compatible interface ( ~v0.6 )
  *
  * - UNCHECKED -> Tx status has not been checked and there's no information about it.
  * - PROCESSING -> Tx checks are in place until a resolution happens: OK, INDETERMINATE, ERROR.
- * - OK -> Relay received the Tx && all downstream miners accepted without complains && tx mined sucessfully
+ * - OK -> Relay received the Tx && all downstream miners accepted without complains && tx mined successfully
  * - INDETERMINATE -> Relay received correctly the Tx && at least one miner accepted the TX && TX potentially mineable
- * - ERROR -> Relay hasen't received the TX || none of the miners accepted the Tx || Tx was not mined sucessfully
+ * - ERROR -> Relay hasn't received the TX || none of the miners accepted the Tx || Tx was not mined successfully
  *
  */
 
